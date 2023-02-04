cc2ce981c105c013b3b33a041b54d7a7344f45d9
Gohar Abbas <103553225+khadimhussain110@users.noreply.github.com>
2023-02-04 05:19:13 -0800

    Add files via upload
    first js assdi

---
+++ app.js
@@ -0,0 +1,26 @@
+// Assignment # 1
+// JAVASCRIPT
+
+// question 1
+
+// alert("Error! Please enter a valid passward");
+
+// question 2
+
+// var a =("enter your name");
+// alert(a)
+
+// question 3
+
+// var data =("my name is gohar abbas \n i am from pakistan");
+// alert(data);
+
+// question 4
+
+// var data2 =("Welcomr to js Land");
+// alert(data2);
+// alert("hello sir how are you doing now a days")
+
+// question 5
+var data3 =("hello... I can run js through my web browser's console")
+console.log(data3);
